75c42642411b2bb269943b20ffe79c357e4a330d
unknown <luuhung261125@gmail.com>
2025-06-24 11:14:37 +0700

    v1

---
+++ Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace TrienKhaiPhanMem3.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
